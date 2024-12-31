271e63c76d2d914ef457968371dbc7a80a224fc1
Truong-lq <truonglq@zigexn.vn>
2024-12-31 16:14:49 +0700

    config production

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module FoodDataApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << "team16.zigexn.vn"
   end
 end
